a9adf489388314c68eb27ebb6e057f9d6b65819e
tugas <aldayanuar6@gmail.com>
2025-11-14 23:01:37 +0700

    admin, image, css

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 // config/database.php
 $host = "localhost";
-$port = "5432";
-$dbname = "lab_ai";
+$port = "5433";
+$dbname = "lab-ai";
 $user = "postgres";
-$password = "12345678"; // ubah sesuai konfigurasi kamu
+$password = "12345678";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
