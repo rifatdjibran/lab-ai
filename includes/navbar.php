51dbb8d2c7c3959d6804d5f14639867eaa721b02
aadinda75 <dindaluluk209@gmail.com>
2025-11-18 00:06:21 +0700

    navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
     <div class="container-fluid">
-    <a class="navbar-brand d-flex align-items-center" href="index.php">
-    <img src="assets/img/logo.png" alt="Logo" height="40" class="me-2">
+    <a class="navbar-brand d-flex align-items-center" href="/lab-ai/index.php">
+        <img src="/lab-ai/assets/img/logo.png" alt="Logo" height="40" class="me-2">
     </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -10,30 +10,31 @@
 
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto align-items-lg-center">
-        <li class="nav-item"><a class="nav-link active" href="../index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="public/struktur.php">Struktur Organisasi</a></li>
+
+        <li class="nav-item"><a class="nav-link" href="/lab-ai/index.php">Home</a></li>
+
+        <li class="nav-item"><a class="nav-link" href="/lab-ai/public/struktur.php">Struktur Organisasi</a></li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="penelitianPublikasiDropdown" role="button" data-bs-toggle="dropdown">
             Penelitian & Publikasi
           </a>
           <ul class="dropdown-menu" aria-labelledby="penelitianPublikasiDropdown">
-            <li><a class="dropdown-item" href="public/fasilitas.php#penelitian">Hasil Penelitian</a></li>
-            <li><a class="dropdown-item" href="public/kegiatan.php#publikasi">Publikasi Ilmiah</a></li>
+            <li><a class="dropdown-item" href="/lab-ai/public/penelitian.php#penelitian">Hasil Penelitian</a></li>
+            <li><a class="dropdown-item" href="/lab-ai/public/kegiatan.php#publikasi">Publikasi Ilmiah</a></li>
           </ul>
         </li>
 
-        <li class="nav-item"><a class="nav-link" href="../public/fasilitas.php">Fasilitas</a></li>
-        <li class="nav-item"><a class="nav-link" href="../public/agenda.php">Agenda</a></li>
-        <li class="nav-item"><a class="nav-link" href="../public/galery.php">Gallery</a></li>
-        <li class="nav-item"><a class="nav-link" href="../public/berita.php">Berita</a></li>
-        <li class="nav-item"><a class="nav-link" href="public/kontak.php">Kontak</a></li>
+        <li class="nav-item"><a class="nav-link" href="/lab-ai/public/fasilitas.php">Fasilitas</a></li>
+        <li class="nav-item"><a class="nav-link" href="/lab-ai/public/agenda.php">Agenda</a></li>
+        <li class="nav-item"><a class="nav-link" href="/lab-ai/public/kontak.php">Kontak</a></li>
 
-        <!-- Tombol Login Admin -->
         <li class="nav-item ms-lg-3">
-          <a href="admin/login.php" class="btn btn-outline-primary btn-sm px-3">
+          <a href="/lab-ai/admin/login.php" class="btn btn-outline-primary btn-sm px-3">
             <i class="bi bi-person-lock me-1"></i> Login Admin
           </a>
         </li>
+
       </ul>
     </div>
   </div>
